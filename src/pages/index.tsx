d64f8b9563cedb6b768a090bd495a161c025b97c
Ederson <ederson.ufrgs@gmail.com>
2021-03-05 16:59:43 -0300

    start initial with typescript, eslint and prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Wellcome</h1>
+      </main>
+      <footer></footer>
+    </div>
+  )
+}
